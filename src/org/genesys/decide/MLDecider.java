ba6609186df955742d8cc0cf94417725ceba3227
Ruben Martins <rmartins@cs.utexas.edu>
2017-06-06 11:09:56 -0500

    Interface with the SAT4J solver

--- src/org/genesys/decide/MLDecider.java
+++ src/org/genesys/decide/MLDecider.java
@@ -1,8 +1,16 @@
 package org.genesys.decide;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by yufeng on 5/31/17.
  */
 public class MLDecider implements Decider {
 
+
+    @Override
+    public String decide(List<String> trail, List<String> candidates) {
+        return "";
+    }
 }
