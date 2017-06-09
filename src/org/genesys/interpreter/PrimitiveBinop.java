305f3e45d454e329d0fa2ac1ed9c2e78dc642567
Yu Feng <fengyu8299@gmail.com>
2017-06-09 02:38:27 -0500

    fix a bug

--- src/org/genesys/interpreter/PrimitiveBinop.java
+++ src/org/genesys/interpreter/PrimitiveBinop.java
@@ -3,36 +3,41 @@ package org.genesys.interpreter;
 /**
  * Created by yufeng on 5/31/17.
  */
-public class PrimitiveBinop {
+public class PrimitiveBinop implements Binop {
     private final String op;
+
     public PrimitiveBinop(String op) {
         this.op = op;
     }
+
     public Object apply(Object first, Object second) {
-        if(this.op.equals("+")) {
-            return (int)first + (int)second;
-        } else if(this.op.equals("*")) {
-            return (int)first * (int)second;
-        } else if(this.op.equals(">")) {
-            return (int)first > (int)second;
-        } else if(this.op.equals(">=")) {
-            return (int)first >= (int)second;
-        } else if(this.op.equals("<")) {
-            return (int)first < (int)second;
-        } else if(this.op.equals("<=")) {
-            return (int)first <= (int)second;
-        } else if(this.op.equals("||")) {
-            return (boolean)first || (boolean)second;
-        } else if(this.op.equals("&&")) {
-            return (boolean)first && (boolean)second;
-        } else if(this.op.equals("-")) {
-            return -(int)first;
-        } else if(this.op.equals("~")) {
-            return !(boolean)first;
+        if (this.op.equals("+")) {
+            return (int) first + (int) second;
+        } else if (this.op.equals("*")) {
+            return (int) first * (int) second;
+        } else if (this.op.equals(">")) {
+            return (int) first > (int) second;
+        } else if (this.op.equals(">=")) {
+            return (int) first >= (int) second;
+        } else if (this.op.equals("<")) {
+            return (int) first < (int) second;
+        } else if (this.op.equals("<=")) {
+            return (int) first <= (int) second;
+        } else if (this.op.equals("||")) {
+            return (boolean) first || (boolean) second;
+        } else if (this.op.equals("&&")) {
+            return (boolean) first && (boolean) second;
+        } else if (this.op.equals("-")) {
+            return -(int) first;
+        } else if (this.op.equals("~")) {
+            return !(boolean) first;
+        } else if (this.op.equals("%")) {
+            return (int) first % (int) second;
         } else {
             throw new RuntimeException();
         }
     }
+
     @Override
     public String toString() {
         return "l(a,b).(" + this.op + " a b)";
