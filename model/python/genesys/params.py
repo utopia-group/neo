d2fcf5e0ca7b41796a2b219e9fa9e475cfce2141
Osbert Bastani <obastani@cs.stanford.edu>
2017-08-26 10:40:24 -0400

    refactored training

---
+++ model/python/genesys/params.py
@@ -0,0 +1,24 @@
+# Input/output value parameters
+num_vals = 22             # values are ints in [-10, 10]
+val_embedding_dim = 20    # dimension of the vector embedding of values
+input_length = 5          # lengths of input lists
+output_length = 5         # lengths of output lists
+
+# DSL Operator parameters
+num_dsl_ops = 44          # number of DSL operators
+dsl_op_embedding_dim = 20 # dimension of the vector embedding of DSL operators
+n_gram_length = 2         # length of DSL operator n-grams
+
+# Neural net parameters
+hidden_layer_dim = 256    # number of nodes in each hidden layer)
+
+# Train parameters
+num_epochs = 100
+batch_size = 50
+step_size = 1e-4
+save_path = 'deep_coder_model.ckpt'
+load_prev = True
+
+# Dataset parameters
+l2_dataset_filename = 'deep_coder.txt'
+train_frac = 0.7
--- model/python/genesys/train.py
+++ model/python/genesys/train.py
@@ -1,38 +1,11 @@
 from data import *
 from nn import *
+from params import *
 
 def main():
-
-    # Step 1: Parameters
-
-    # Step 1a: Input/output value parameters
-    num_vals = 22             # values are ints in [-10, 10]
-    val_embedding_dim = 20    # dimension of the vector embedding of values
-    input_length = 5          # lengths of input lists
-    output_length = 5         # lengths of output lists
-
-    # Step 1b: DSL Operator parameters
-    num_dsl_ops = 44          # number of DSL operators
-    dsl_op_embedding_dim = 20 # dimension of the vector embedding of DSL operators
-    n_gram_length = 2         # length of DSL operator n-grams
-
-    # Step 1c: Neural net parameters
-    hidden_layer_dim = 256    # number of nodes in each hidden layer)
-
-    # Step 1d: Build model parameters
+    # Step 1: Build parameters
     model_params = DeepCoderModelParams(num_vals, val_embedding_dim, input_length, output_length, num_dsl_ops, dsl_op_embedding_dim, n_gram_length, hidden_layer_dim)
-
-    # Step 1e: Train parameters
-    num_epochs = 100
-    batch_size = 50
-    step_size = 1e-4
-    save_path = 'deep_coder_model.ckpt'
-    load_prev = True
     train_params = DeepCoderTrainParams(num_epochs, batch_size, step_size, save_path, load_prev)
-
-    # Step 1f: Dataset parameters
-    l2_dataset_filename = 'deep_coder.txt'
-    train_frac = 0.7
 
     # Step 2: Build neural net
     model = DeepCoderModel(model_params)
