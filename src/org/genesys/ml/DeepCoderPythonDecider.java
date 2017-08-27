179c226ed16322212dcbd3c9219301682f65b0a0
Osbert Bastani <obastani@cs.stanford.edu>
2017-08-27 15:42:19 -0400

    updated python nn decider to batch computations for all possible n-grams

--- src/org/genesys/ml/DeepCoderPythonDecider.java
+++ src/org/genesys/ml/DeepCoderPythonDecider.java
@@ -5,7 +5,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.genesys.decide.Decider;
 import org.genesys.models.Trio;
@@ -13,26 +16,28 @@ import org.genesys.models.Trio;
 public class DeepCoderPythonDecider implements Decider {
 	private static final String FILENAME = "./model/tmp/deep_coder.txt";
 	private static final String COMMAND = "/usr/local/bin/python -m model.genesys.test";
+	private static final int N_GRAM_LENGTH = 2;
 	
 	private final XFeaturizer<Object> xFeaturizer;
 	private final YFeaturizer yFeaturizer;
 	private final Object input;
 	private final Object output;
 	
+	private final Map<String,double[]> probabilities = new HashMap<String,double[]>();
+	
 	public DeepCoderPythonDecider(XFeaturizer<Object> xFeaturizer, YFeaturizer yFeaturizer, Object input, Object output) {
 		this.xFeaturizer = xFeaturizer;
 		this.yFeaturizer = yFeaturizer;
 		this.input = input;
 		this.output = output;
+		
+		this.build();
 	}
 	
 	@Override
 	public String decide(List<String> ancestors, List<String> functionChoices) {
 		// Step 1: Get the probabilities
-		double[] probabilities = this.runPythonScript(ancestors);
-		if(probabilities.length != this.yFeaturizer.functions.size()+1) { // +1 is for NO_FUNCTION option
-			throw new RuntimeException("Invalid result length!");
-		}
+		double[] probabilities = this.probabilities.get(NGramDecider.getNGram(ancestors, N_GRAM_LENGTH));
 		
 		// Step 2: Get the most likely function
 		String maxFunction = null;
@@ -47,47 +52,74 @@ public class DeepCoderPythonDecider implements Decider {
 		return maxFunction;
 	}
 	
-	private double[] runPythonScript(List<String> ancestors) {
-		// Step 1: Build the datapoint
-		Trio<List<Integer>,List<Integer>,List<Integer>> features = this.xFeaturizer.getFeatures(ancestors, this.input, this.output);
-		String datapoint = "(" + Utils.toString(features.t0) + ", " + Utils.toString(features.t1) + ", " + Utils.toString(features.t2) + ")";
-		
-		// Step 2: Print to file
+	private void build() {
+		// Step 1: Build the test set
+		List<String> nGrams = new ArrayList<String>();
 		try {
 			PrintWriter pw = new PrintWriter(new FileWriter(FILENAME));
-			pw.print(datapoint);
+			
+			for(String function0 : this.yFeaturizer.functions) {
+				for(String function1 : this.yFeaturizer.functions) {
+					// Step 1a: Build the n-gram
+					List<String> nGram = new ArrayList<String>();
+					nGram.add(function0);
+					nGram.add(function1);
+					
+					// Step 1b: Build the datapoint
+					Trio<List<Integer>,List<Integer>,List<Integer>> features = this.xFeaturizer.getFeatures(nGram, this.input, this.output);
+					String datapoint = "(" + Utils.toString(features.t0) + ", " + Utils.toString(features.t1) + ", " + Utils.toString(features.t2) + ")";
+					
+					// Step 1c: Print to test set file
+					pw.println(datapoint);
+					
+					// Step 1d: Add the n-gram to the list
+					nGrams.add(Utils.toString(nGram));
+				}
+			}
+			
 			pw.close();
 		} catch(IOException e) {
 			throw new RuntimeException(e);
 		}
 		
-		// Step 3: Execute the python script
-		String[] tokens = null;
+		// Step 2: Get the neural net output
+		List<double[]> rawResults = new ArrayList<double[]>();
 		try {
+			// Step 2a: Execute the Python script
 			Process p = Runtime.getRuntime().exec(COMMAND);
+			
+			// Step 2b: Read the output
 			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String line;
 			while((line = br.readLine()) != null) {
 				if(line.startsWith("RESULT: ")) {
-					tokens = line.substring(9, line.length()-1).split(", ");
+					String[] tokens = line.substring(9, line.length()-1).split(", ");
+					double[] rawResult = new double[tokens.length];
+					for(int i=0; i<tokens.length; i++) {
+						rawResult[i] = Double.parseDouble(tokens[i]);
+					}
+					if(rawResult.length != this.yFeaturizer.functions.size() + 1) {
+						throw new RuntimeException("Invalid number of probabilities!");
+					}
+					rawResults.add(rawResult);
 				}
 			}
 			br.close();
+			
+			// Step 2c: Append the output
+			
+			// Step 2c: Wait for the process to finish
 			p.waitFor();
 		} catch (IOException | InterruptedException e) {
 			throw new RuntimeException(e);
 		}
 		
-		if(tokens == null) {
+		// Step 3: Build the mapping
+		if(nGrams.size() != rawResults.size()) {
 			throw new RuntimeException();
 		}
-		
-		// Step 4: Convert to float array
-		double[] result = new double[tokens.length];
-		for(int i=0; i<tokens.length; i++) {
-			result[i] = Double.parseDouble(tokens[i]);
+		for(int i=0; i<nGrams.size(); i++) {
+			this.probabilities.put(nGrams.get(i), rawResults.get(i));
 		}
-		
-		return result;
 	}
 }
--- src/org/genesys/ml/NGramDecider.java
+++ src/org/genesys/ml/NGramDecider.java
@@ -9,22 +9,23 @@ import org.genesys.models.Node;
 import org.genesys.models.Pair;
 
 public class NGramDecider implements Decider {
+	private static final String NO_FUNCTION = "";
+	
 	private final int nGramLength;
 	private final Counter<Pair<String,String>> counts = new Counter<Pair<String,String>>();
-	private final String NO_FUNCTION = "";
 	
 	public NGramDecider(List<Node> programs, int nGramLength) {
 		this.nGramLength = nGramLength;
 		for(Node program : programs) {
 			for(Pair<String,List<String>> ancestor : DatasetGenerator.getFunctionData(program)) {
-				this.counts.increment(new Pair<String,String>(this.getNGram(ancestor.t1), ancestor.t0));
+				this.counts.increment(new Pair<String,String>(getNGram(ancestor.t1, this.nGramLength), ancestor.t0));
 			}
 		}
 	}
 	
-	private String getNGram(List<String> ancestors) {
+	public static String getNGram(List<String> ancestors, int nGramLength) {
 		List<String> nGram = new ArrayList<String>();
-		for(int i=0; i<this.nGramLength; i++) {
+		for(int i=0; i<nGramLength; i++) {
 			String curFunction = ancestors.size() > i ? ancestors.get(ancestors.size() - i - 1) : NO_FUNCTION;
 			nGram.add(curFunction);
 		}
@@ -35,7 +36,7 @@ public class NGramDecider implements Decider {
 	public String decide(List<String> ancestors, List<String> functionChoices) {
 		String curFunction = null;
 		int curCount = 0;
-		String nGram = getNGram(ancestors);
+		String nGram = getNGram(ancestors, this.nGramLength);
 		for(String function : functionChoices) {
 			int count = this.counts.getCount(new Pair<String,String>(nGram, function));
 			if(curFunction == null || count > curCount) {
